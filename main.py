3cf8ba0795746e2d5607ed9222f6f802d1fd0e94
Zeexel <jamestp92050@gmail.com>
2021-02-03 17:32:42 -0700

    Clean up settings.json

--- main.py
+++ main.py
@@ -13,21 +13,20 @@ coloredlogs.install('DEBUG', logger=logger)
 settings = j.load(open('settings.json', 'r'))
 
 
-# TODO: Learn async.. Whoops.
 async def main():
     logger.info('Starting Forecast Gen V2..')
     output = ""
     outfile = open('output.txt', "w+")
 
-    # TODO: Add check for latest version from GitHub    -- can probably start on this soon
     # TODO: Use async to run getActiveAlerts every now and then and check for new alerts
-    for z in settings['Zones']:
+    for z in settings['ForecastGenSettings']['Zones']:
         logger.debug(f'OBTAIN ZONE FORECAST FOR ZONE {z}')
         output += nwshandler.forecast(z=z)
         output += str(nwshandler.getActiveAlerts(z=z))
 
     outfile.write(output)
-    # SpeechHandler.balcon(volume=25, rate=250, text='-f ..\\output.txt')
+    logger.info("Wrote text file in /forecastgen-v2/output.txt")
+    # SpeechHandler.balcon(volume=25, rate=250, fileLocation='-f ../output.txt')
     # SpeechHandler.dectalk()
 
 
