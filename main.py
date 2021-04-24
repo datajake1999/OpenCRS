13887cdadc0964ba3859fbf76e6a082924368215
Zeexel <jamestp92050@gmail.com>
2021-04-23 22:45:11 -0700

    Small changes

--- main.py
+++ main.py
@@ -21,16 +21,17 @@ async def main():
 
     # TODO: Use async to run getActiveAlerts every now and then and check for new alerts
     for z in settings['OpenCRSsettings']['Zones']:
-        logger.debug(f'OBTAIN ZONE FORECAST FOR ZONE {z}')
+        logger.info(f'Getting forecast data from zone {z}')
         output += nwshandler.forecast(z=z)
         output += nwshandler.getActiveAlerts(z=z)
 
     output += (f"Here are the current observations, as of {time.strftime('%I:%M %p %Z')}.\n")
 
     for s in settings['OpenCRSsettings']['ObservationZones']:
-        logger.debug(f"OBTAIN DATA FROM STATION {s}")
+        logger.info(f"Getting data from zone {s}")
         output += nwshandler.getObservation(s=s)
 
+    
 
     outfile.write(output)
     outfile.close()
@@ -39,8 +40,8 @@ async def main():
     # TTS
     TTSoptions = settings["TTS"]
     if TTSoptions["enabled"]:
-        logger.info("Note that TTS implementation is very janky! It relies on DECTalk or Balabolka, as well as your own installed TTS voices.")
-        logger.info("Check README for instructions on using Balcon/DECTalk with Forecast-Gen!")
+        logger.warning("Note that TTS implementation is very janky! It relies on DECTalk or Balabolka, as well as your own installed TTS voices.")
+        logger.warning("Check README for instructions on using Balcon/DECTalk with Forecast-Gen!")
 
         TTSengine = TTSoptions['engine']
         DTSettings = TTSoptions['dectalk']
@@ -56,21 +57,21 @@ async def main():
 
 if __name__ == '__main__':
     noaa = r.get('https://api.weather.gov')
-    # github = r.get('https://api.github.com/repos/Zeexel/forecastgen-v2/releases/latest').json()
 
-    # Code below should be commented until the first release of forecastgen-v2
-    # Github release check
-    """
+    #Github release check
     ver = settings['version']
-    if github['name'] != ver:
-        logger.warning("You are not using the latest version of forecastgen! Latest version: {github['name']}")
-    else:
-        logger.info("Using the latest build of forecastgen!")
-        """
+    try:
+        github = r.get('https://api.github.com/repos/Zeexel/OpenCRS/releases/latest').json()
+        if github['name'] != ver:
+            logger.warning("You are not using the latest version of forecastgen! Latest version: {github['name']}")
+        else:
+            logger.info("Using the latest build of OpenCRS!")
+    except Exception:
+        logger.error("Could not obtain the latest version of OpenCRS! (Probably cause there isn't a release build yet..)")
 
     # Check NOAA status
     if noaa.ok:
         logger.info("NWS Api is up!")
-        asyncio.run(main())
+        asyncio.get_event_loop().run_until_complete(main())
     else:
         logger.critical("Couldn't get a 200 from NWS! Check to see if you're connected to the internet.")
